7892ec26bd7b6a7ac0713402bea58b1ea2acc128
Casey <Casey@DESKTOP-927IVMI>
2020-08-31 17:44:58 +0800

    init

---
+++ src/main/java/pers/ljy/download/ngastack/toexcel/DownloadNgaStockToexcelApplication.java
@@ -0,0 +1,13 @@
+package pers.ljy.download.ngastack.toexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadNgaStockToexcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadNgaStockToexcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/ljy/download/ngastack/toexcel/DownloadNgaStockToexcelApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.ljy.download.ngastack.toexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadNgaStockToexcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
